fa16959c74b309f66c7081b78f0ffa3601f988ea
ym652324 <1430551121@qq.con>
2019-12-04 23:01:17 +0800

    配置pom文档及mybatis

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -2,6 +2,7 @@ package com.miaoshaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App 
 {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello world!";
+}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
